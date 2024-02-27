102af2eaf673c61b3a780917ff37c090ed1c7d0e
Ainsley <ainsleyjyl@gmail.com>
2024-02-27 13:45:27 -0800

    add order component

--- pizza-menu/src/components/Order.jsx
+++ pizza-menu/src/components/Order.jsx
@@ -1,3 +1,10 @@
 export default function Order({ openHour, closeHour }) {
-    return <div className=''></div>;
+    return (
+        <div className='order'>
+            <p>
+                We're open from {openHour}:00 to {closeHour}:00. Come visit us or order online.
+            </p>
+            <button className='btn'>Order</button>
+        </div>
+    );
 }
