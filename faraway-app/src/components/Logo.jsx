96fb8d45807d9b2d6912f5c83d597c4ee6a5641c
Ainsley <ainsleyjyl@gmail.com>
2024-03-01 20:20:16 -0800

    add components

---
+++ faraway-app/src/components/Logo.jsx
@@ -0,0 +1,3 @@
+export default function Logo() {
+    return <h1>🏝🏝 Far Away ⛱</h1>;
+}
---
+++ faraway-app/src/components/Stats.jsx
@@ -0,0 +1,7 @@
+export default function Stats() {
+    return (
+        <div className='add-form'>
+            <h3>What do you need for your trip 😍</h3>
+        </div>
+    );
+}
