d323f0a7fa69ca467a7ea176ba37da346add18a4
Ainsley <ainsleyjyl@gmail.com>
2024-03-01 20:41:17 -0800

    add data

---
+++ faraway-app/src/components/Item.jsx
@@ -0,0 +1,10 @@
+export default function Item({ item }) {
+    return (
+        <li className=''>
+            <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
+}
--- faraway-app/src/components/Stats.jsx
+++ faraway-app/src/components/Stats.jsx
@@ -1,6 +1,6 @@
 export default function Stats() {
     return (
-        <footer>
+        <footer className='stats'>
             <em>You have x items on your list, and you have already packed x</em>
         </footer>
     );
