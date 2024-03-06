37249786c8dc7d95e16916bbe6a52f07e12cdac0
Ainsley <ainsleyjyl@gmail.com>
2024-03-05 21:56:14 -0800

    add functionality to buttons

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import Button from './components/Button';
 import FormAddFriend from './components/FormAddFriend';
 import FormSplitBill from './components/FormSplitBill';
 import FriendList from './components/FriendList';
 
 function App() {
+    const [showAddForm, setShowAddForm] = useState(false);
+
+    function handleShow() {
+        setShowAddForm((prevShowAddForm) => !prevShowAddForm);
+    }
+
     return (
         <div className='app'>
             <div className='sidebar'>
                 <FriendList />
-                <FormAddFriend />
-                <Button>Add Friend</Button>
+                {showAddForm && <FormAddFriend />}
+                <Button onClick={handleShow}>{showAddForm ? 'Close' : 'Add Friend'}</Button>
             </div>
             <FormSplitBill />
         </div>
