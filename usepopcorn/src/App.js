5012f5d84798eccf254905d3408a259b9599d343
Ainsley <ainsleyjyl@gmail.com>
2024-03-09 17:35:58 -0800

    use component composition

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,13 +1,26 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
+import Search from './components/Search';
+import NumResults from './components/NumResults';
 import Main from './components/Main';
+import ListBox from './components/ListBox';
+import MovieList from './components/MovieList';
+import WatchedBox from './components/WatchedBox';
 import tempMovieData from './movieData';
 export default function App() {
     const [movies, setMovies] = useState(tempMovieData);
     return (
         <>
-            <Navbar movies={movies} />
-            <Main movies={movies} />
+            <Navbar>
+                <Search />
+                <NumResults movies={movies} />
+            </Navbar>
+            <Main>
+                <ListBox>
+                    <MovieList movies={movies} />
+                </ListBox>
+                <WatchedBox />
+            </Main>
         </>
     );
 }
