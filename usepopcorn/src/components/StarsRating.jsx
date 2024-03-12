aa575bacc64a8402289f7ae10f3927edea1ae5f4
Ainsley <ainsleyjyl@gmail.com>
2024-03-12 12:54:07 -0700

    add reusable Star component and use prop-types

---
+++ usepopcorn/src/components/StarsRating.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import Star from './Star';
+import PropTypes from 'prop-types';
+
+const containerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '16px',
+};
+
+const starStyle = {
+    display: 'flex',
+};
+
+StarsRating.propTypes = {
+    maxRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    defaultRating: PropTypes.number,
+};
+
+export default function StarsRating({
+    maxRating = 10,
+    color = '#fcc419',
+    size = 48,
+    defaultRating = 0,
+}) {
+    const [rating, setRating] = useState(defaultRating);
+    const [tempRating, setTempRating] = useState(1);
+
+    const textStyle = {
+        lineHeight: '1',
+        margin: '0',
+        color,
+        fontSize: `${size / 1.5}px`,
+    };
+    return (
+        <div style={containerStyle}>
+            <div style={starStyle}>
+                {Array.from({ length: maxRating }, (_, i) => (
+                    <Star
+                        key={i}
+                        onRating={() => setRating(i + 1)}
+                        full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+                        onHoverIn={() => setTempRating(i + 1)}
+                        onHoverOut={() => setRating(0)}
+                        color={color}
+                        size={size}
+                    />
+                ))}
+            </div>
+            <p style={textStyle}>{tempRating || rating || ''}</p>
+        </div>
+    );
+}
--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import StarsRating from './components/StarsRating';
+// import './index.css';
+// import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <StarsRating maxRating={5} />
+        <StarsRating maxRating={10} />
+        {/* <App /> */}
     </React.StrictMode>
 );
