92a8bdda6256475fc90f28d9ae2caa2a5d84c280
Ainsley <ainsleyjyl@gmail.com>
2024-03-15 12:52:18 -0700

    set focus on search input

--- usepopcorn/src/components/Search.jsx
+++ usepopcorn/src/components/Search.jsx
@@ -1,4 +1,25 @@
+import { useEffect } from 'react';
+import { useRef } from 'react';
+
 export default function Search({ query, setQuery }) {
+    const inputEl = useRef(null);
+
+    useEffect(() => {
+        // use keydown
+        function callback(e) {
+            // if input still on focus press return key wont do anything
+            if (document.activeElement === inputEl.current) return;
+            if (e.code === 'Enter') {
+                // taget current property
+                inputEl.current.focus();
+                // set return key to empty input
+                setQuery('');
+            }
+        }
+        document.addEventListener('keydown', callback);
+        return () => document.addEventListener('keydown', callback);
+    }, [setQuery]);
+
     return (
         <input
             className='search'
@@ -6,6 +27,7 @@ export default function Search({ query, setQuery }) {
             placeholder='Search movies...'
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            ref={inputEl}
         />
     );
 }
