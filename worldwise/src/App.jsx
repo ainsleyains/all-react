2179936752cac1b2548c78332820e9aa41d9ae68
Ainsley <ainsleyjyl@gmail.com>
2024-03-20 13:27:07 -0700

    add pages

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,3 +1,32 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Home from './pages/Home';
+import PageNotFound from './pages/PageNotFound';
+
 export default function App() {
-    return <div className=''>Worldwise</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path='product'
+                    element={<Product />}
+                />
+
+                <Route
+                    path='pricing'
+                    element={<Pricing />}
+                />
+                <Route
+                    path='/'
+                    element={<Home />}
+                />
+
+                <Route
+                    path='*'
+                    element={<PageNotFound />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
