2ab522fa8c7fcf27422eb7c4978f2ce5703cafcc
Ainsley <ainsleyjyl@gmail.com>
2024-03-24 12:17:51 -0700

    add custom hook

--- atomic-blog/src/PostContext.js
+++ atomic-blog/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { faker } from '@faker-js/faker';
 
 function createRandomPost() {
@@ -45,4 +45,11 @@ function PostProvider({ children }) {
     );
 }
 
-export { PostProvider, PostContext };
+//custom hook
+function usePosts() {
+    const context = useContext(PostContext);
+    if (context === undefined) throw new Error('PostContext was used outside of the PostProvider');
+    return context;
+}
+
+export { PostProvider, usePosts };
