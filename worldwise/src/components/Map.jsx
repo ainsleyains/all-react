5e2db99aa4b016332f97254211c509a24d1ae02f
Ainsley <ainsleyjyl@gmail.com>
2024-03-25 11:18:03 -0700

    get marker on map

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -3,9 +3,24 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
+
+const flagemojiToPNG = (flag) => {
+    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+        .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+        .join('');
+    return (
+        <img
+            src={`https://flagcdn.com/24x18/${countryCode}.png`}
+            alt='flag'
+        />
+    );
+};
 
 export default function Map() {
     const navigate = useNavigate();
+    const { cities } = useCities();
+
     const [mapPosition, setMapPosition] = useState([40, 0]);
     const [searchParams, setSearchParams] = useSearchParams();
 
@@ -29,11 +44,17 @@ export default function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {cities.map((city) => (
+                    <Marker
+                        position={[city.position.lat, city.position.lng]}
+                        key={city.id}
+                    >
+                        <Popup>
+                            <span>{flagemojiToPNG(city.emoji)}</span>
+                            <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     );
--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -50,4 +50,5 @@ function useCities() {
     return context;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export { CitiesProvider, useCities };
