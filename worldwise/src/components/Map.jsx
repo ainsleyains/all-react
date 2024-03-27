9e70d48888d16e6ee4cbbf853da250180a1ed754
Ainsley <ainsleyjyl@gmail.com>
2024-03-26 21:36:57 -0700

    set map position with geolocation

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -4,6 +4,8 @@ import styles from './Map.module.css';
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
 
 const flagemojiToPNG = (flag) => {
     var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
@@ -19,9 +21,9 @@ const flagemojiToPNG = (flag) => {
 
 export default function Map() {
     const { cities } = useCities();
-
     const [mapPosition, setMapPosition] = useState([40, 0]);
     const [searchParams, setSearchParams] = useSearchParams();
+    const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation();
 
     const mapLat = searchParams.get('lat');
     const mapLng = searchParams.get('lng');
@@ -30,8 +32,24 @@ export default function Map() {
         if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
     }, [mapLat, mapLng]);
 
+    useEffect(
+        function () {
+            if (geolocationPosition) setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+        },
+        [geolocationPosition]
+    );
+
     return (
         <div className={styles.mapContainer}>
+            {!geolocationPosition && (
+                <Button
+                    type='position'
+                    onClick={getPosition}
+                >
+                    {isLoadingPosition ? 'Loading...' : 'Use your position'}
+                </Button>
+            )}
+
             <MapContainer
                 center={mapPosition}
                 zoom={6}
