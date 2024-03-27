ff8fd739527118a75f5185b01cf7b22e751bbb48
Ainsley <ainsleyjyl@gmail.com>
2024-03-26 20:05:12 -0700

    fix bugs

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
+import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
 
 const flagemojiToPNG = (flag) => {
@@ -18,25 +18,23 @@ const flagemojiToPNG = (flag) => {
 };
 
 export default function Map() {
-    const navigate = useNavigate();
     const { cities } = useCities();
 
     const [mapPosition, setMapPosition] = useState([40, 0]);
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
+    const mapLat = searchParams.get('lat');
+    const mapLng = searchParams.get('lng');
+
+    useEffect(() => {
+        if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    }, [mapLat, mapLng]);
 
     return (
-        <div
-            className={styles.mapContainer}
-            onClick={() => {
-                navigate('form');
-            }}
-        >
+        <div className={styles.mapContainer}>
             <MapContainer
                 center={mapPosition}
-                zoom={13}
+                zoom={6}
                 scrollWheelZoom={true}
                 className={styles.map}
             >
@@ -55,7 +53,23 @@ export default function Map() {
                         </Popup>
                     </Marker>
                 ))}
+                <ChangeCenter position={mapPosition} />
+                <DetecClick />
             </MapContainer>
         </div>
     );
+}
+
+// eslint-disable-next-line react/prop-types
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position);
+    return null;
+}
+
+function DetecClick() {
+    const navigate = useNavigate();
+    useMapEvents({
+        click: (e) => navigate(`form`),
+    });
 }
