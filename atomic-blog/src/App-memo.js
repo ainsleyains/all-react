1239c9fcf95300ea8b1b035360f5f5907807eafb
Ainsley <ainsleyjyl@gmail.com>
2024-03-29 12:47:20 -0700

    add useMemo

--- atomic-blog/src/App-memo.js
+++ atomic-blog/src/App-memo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, memo } from 'react';
+import { useEffect, useState, memo, useMemo } from 'react';
 import { faker } from '@faker-js/faker';
 
 function createRandomPost() {
@@ -35,6 +35,13 @@ function App() {
         [isFakeDark]
     );
 
+    const archiveOptions = useMemo(() => {
+        return {
+            show: false,
+            title: `Post archive in addition to ${posts.length}main posts`,
+        };
+    }, [posts.length]);
+
     return (
         <section>
             <button
@@ -54,7 +61,7 @@ function App() {
                 posts={searchedPosts}
                 onAddPost={handleAddPost}
             />
-            <Archive show={false} />
+            <Archive archiveOptions={archiveOptions} />
             <Footer />
         </section>
     );
@@ -151,18 +158,18 @@ function List({ posts }) {
     );
 }
 
-const Archive = memo(function Archive({ show }) {
+const Archive = memo(function Archive({ archiveOptions }) {
     // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
     const [posts] = useState(() =>
         // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
         Array.from({ length: 10000 }, () => createRandomPost())
     );
 
-    const [showArchive, setShowArchive] = useState(show);
+    const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
     return (
         <aside>
-            <h2>Post archive</h2>
+            <h2>{archiveOptions.title}</h2>
             <button onClick={() => setShowArchive((s) => !s)}>
                 {showArchive ? 'Hide archive posts' : 'Show archive posts'}
             </button>
