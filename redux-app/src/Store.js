d24b9c60f3a36c82c6f895acb01fb2706ee83936
Ainsley <ainsleyjyl@gmail.com>
2024-04-01 21:31:30 -0700

    update store

--- redux-app/src/Store.js
+++ redux-app/src/Store.js
@@ -1,10 +1,10 @@
-const initialSate = {
+const initialStateAccount = {
     balance: 0,
     loan: 0,
     loanPurpose: '',
 };
 
-function reducer(state = initialSate, action) {
+function accountReducer(state = initialStateAccount, action) {
     switch (action.type) {
         case 'account/deposit':
             return { ...state, balance: state.balance + action.payload };
@@ -12,9 +12,21 @@ function reducer(state = initialSate, action) {
             return { ...state, balance: state.balance - action.payload };
         case 'account/requestLoan':
             if (state.loan > 0) return state;
-            return { ...state, loan: action.payload };
+            // LATER
+            return {
+                ...state,
+                loan: action.payload.amount,
+                loanPurpose: action.payload.purpose,
+                balance: state.balance + action.payload.amount,
+            };
         case 'account/payLoan':
-            return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan };
+            return {
+                ...state,
+                loan: 0,
+                loanPurpose: '',
+                balance: state.balance - state.loan,
+            };
+
         default:
             return state;
     }
