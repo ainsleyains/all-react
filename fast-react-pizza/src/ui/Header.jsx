ee7cb4360d1fb88c9e54a1966a60f51fb3dc3201
Ainsley <ainsleyjyl@gmail.com>
2024-04-10 20:40:53 -0700

    add color

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/SearchOrder';
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder'
 
 export default function Header() {
     return (
-        <header className=''>
-            <Link to='/'>Fast React Pizza Co.</Link>
+        <header className="bg-yellow-500">
+            <Link to="/">Fast React Pizza Co.</Link>
             <SearchOrder />
             <p>Jonas</p>
         </header>
-    );
+    )
 }
