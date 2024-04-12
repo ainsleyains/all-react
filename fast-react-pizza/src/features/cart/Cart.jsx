b55750997d04c45b665af8fb3509f4b37c8c7ef7
Ainsley <ainsleyjyl@gmail.com>
2024-04-11 20:52:03 -0700

    add style to button

--- fast-react-pizza/src/features/cart/Cart.jsx
+++ fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 // const fakeCart = [
 //     {
@@ -29,16 +29,21 @@ function Cart() {
 
     return (
         <div>
-            <Link to='/menu'>&larr; Back to menu</Link>
+            <Link
+                to="/menu"
+                className="text-sm text-blue-500 hover:text-blue-600"
+            >
+                &larr; Back to menu
+            </Link>
 
             <h2>Your cart, %NAME%</h2>
 
             <div>
-                <Link to='/order/new'>Order pizzas</Link>
+                <Link to="/order/new">Order pizzas</Link>
                 <button>Clear cart</button>
             </div>
         </div>
-    );
+    )
 }
 
-export default Cart;
+export default Cart
--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
 // import { useState } from "react";
 
-import { Form, redirect, useNavigation, useActionData } from 'react-router-dom';
-import { createOrder } from '../../services/apiRestaurant';
+import { Form, redirect, useNavigation, useActionData } from 'react-router-dom'
+import { createOrder } from '../../services/apiRestaurant'
 
 // https://uibakery.io/regex-library/phone-number
-const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
+const isValidPhone = (str) =>
+    /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+        str
+    )
 
 const fakeCart = [
     {
@@ -29,38 +32,30 @@ const fakeCart = [
         unitPrice: 15,
         totalPrice: 15,
     },
-];
+]
 
 function CreateOrder() {
     // const [withPriority, setWithPriority] = useState(false);
-    const navigation = useNavigation();
-    const isSubmitting = navigation.state === 'submitting';
-    const formErrors = useActionData();
+    const navigation = useNavigation()
+    const isSubmitting = navigation.state === 'submitting'
+    const formErrors = useActionData()
 
-    const cart = fakeCart;
+    const cart = fakeCart
 
     return (
         <div>
             <h2>Ready to order? Let&apos;s go!</h2>
 
-            <Form method='POST'>
+            <Form method="POST">
                 <div>
                     <label>First Name</label>
-                    <input
-                        type='text'
-                        name='customer'
-                        required
-                    />
+                    <input type="text" name="customer" required />
                 </div>
 
                 <div>
                     <label>Phone number</label>
                     <div>
-                        <input
-                            type='tel'
-                            name='phone'
-                            required
-                        />
+                        <input type="tel" name="phone" required />
                     </div>
                     {formErrors?.phone && <p>{formErrors.phone}</p>}
                 </div>
@@ -68,55 +63,59 @@ function CreateOrder() {
                 <div>
                     <label>Address</label>
                     <div>
-                        <input
-                            type='text'
-                            name='address'
-                            required
-                        />
+                        <input type="text" name="address" required />
                     </div>
                 </div>
 
                 <div>
                     <input
-                        type='checkbox'
-                        name='priority'
-                        id='priority'
+                        type="checkbox"
+                        name="priority"
+                        id="priority"
                         // value={withPriority}
                         // onChange={(e) => setWithPriority(e.target.checked)}
                     />
-                    <label htmlFor='priority'>Want to yo give your order priority?</label>
+                    <label htmlFor="priority">
+                        Want to yo give your order priority?
+                    </label>
                 </div>
 
                 <div>
                     <input
-                        type='hidden'
-                        name='cart'
+                        type="hidden"
+                        name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <button disabled={isSubmitting}>{isSubmitting ? 'Placing order now' : 'Order now'}</button>
+                    <button
+                        disabled={isSubmitting}
+                        className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+                    >
+                        {isSubmitting ? 'Placing order now' : 'Order now'}
+                    </button>
                 </div>
             </Form>
         </div>
-    );
+    )
 }
 
 export async function action({ request }) {
-    const formData = await request.formData();
-    const data = Object.fromEntries(formData);
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData)
     const order = {
         ...data,
         cart: JSON.parse(data.cart),
         priority: data.priority === 'on',
-    };
+    }
 
-    const errors = {};
-    if (!isValidPhone(order.phone)) errors.phone = 'Please enter correct phone number';
+    const errors = {}
+    if (!isValidPhone(order.phone))
+        errors.phone = 'Please enter correct phone number'
 
-    if (Object.keys(errors).length > 0) return errors;
+    if (Object.keys(errors).length > 0) return errors
 
-    const newOrder = await createOrder(order);
+    const newOrder = await createOrder(order)
 
-    return redirect(`/order/${newOrder.id}`);
+    return redirect(`/order/${newOrder.id}`)
 }
 
-export default CreateOrder;
+export default CreateOrder
