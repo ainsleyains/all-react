34d99d92bd776bb58a4b55771dfec7c7a88311f9
Ainsley <ainsleyjyl@gmail.com>
2024-04-17 11:30:06 -0700

    add cart container

---
+++ rtk-app/src/components/CartContainer.js
@@ -0,0 +1,39 @@
+import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
+
+export default function CartContainer() {
+    const { cartItems, total, amount } = useSelector((store) => store.cart);
+    if (amount < 1)
+        return (
+            <section className='cart'>
+                <header>
+                    <h2>Your bag</h2>
+                    <h4 className='empty-cart'>is currently empty</h4>
+                </header>
+            </section>
+        );
+    return (
+        <section className='cart'>
+            <header>
+                <h2>Your bag</h2>
+            </header>
+            <ul>
+                {cartItems.map((item) => (
+                    <CartItem
+                        key={item.id}
+                        {...item}
+                    />
+                ))}
+            </ul>
+            <footer>
+                <hr />
+                <div className='cart-total'>
+                    <h4>
+                        total <span>${total}</span>
+                    </h4>
+                </div>
+                <button className='btn clear-btn'>Clear Cart</button>
+            </footer>
+        </section>
+    );
+}
