da689d1e9940aeddeeea4b47f0fb467805c02fd3
Ainsley <ainsleyjyl@gmail.com>
2024-04-17 11:37:48 -0700

    add item component

--- rtk-app/src/components/CartItem.js
+++ rtk-app/src/components/CartItem.js
@@ -1,3 +1,26 @@
-export default function CartItem() {
-    return <div className=''>Item</div>;
+import { ChevronDown, ChevronUp } from '../icons';
+
+export default function CartItem({ id, img, title, price, amount }) {
+    return (
+        <article className='cart-item'>
+            <img
+                src={img}
+                alt={title}
+            />
+            <div>
+                <h4>{title}</h4>
+                <h4 className='item-price'>${price}</h4>
+                <button className='remove-btn'>remove</button>
+            </div>
+            <div>
+                <button className='amount-btn'>
+                    <ChevronUp />
+                </button>
+                <p className='amount'>{amount}</p>
+                <button className='amount-btn'>
+                    <ChevronDown />
+                </button>
+            </div>
+        </article>
+    );
 }
