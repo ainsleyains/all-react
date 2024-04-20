32bc732e0d530dd7e92e5f784d5d5ed7292b20be
Ainsley <ainsleyjyl@gmail.com>
2024-04-19 19:52:16 -0700

    add cart slice

---
+++ fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    // cart: [],
+    cart: [
+        {
+            pizzaId: 12,
+            name: 'Romana',
+            quantity: 2,
+            unitPrice: 16,
+            totalPrice: 32,
+        },
+    ],
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        additem(state, action) {
+            state.cart.push(action.payload)
+        },
+        deleteItem(state, action) {
+            state.cart = state.cart.filter(
+                (item) => item.pizzaId !== action.payload
+            )
+        },
+        increaseItemQty(state, action) {
+            const item = state.cart.find(
+                (item) => item.pizzaId === action.payload
+            )
+            item.quantity++
+            item.totalPrice = item.quantity * item.unitPrice
+        },
+        decreaseItemQty(state, action) {
+            const item = state.cart.find(
+                (item) => item.pizzaId === action.payload
+            )
+            item.quantity--
+            item.totalPrice = item.quantity * item.unitPrice
+        },
+        clearCart(state) {
+            state.cart = []
+        },
+    },
+})
+
+export const {
+    additem,
+    deleteItem,
+    increaseItemQty,
+    decreaseItemQty,
+    clearCart,
+} = cartSlice.actions
+
+export default cartSlice.reducer
