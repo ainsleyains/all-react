d8f5d1616f8c968f4472866e1d6cacb0c37c2bf1
Ainsley <ainsleyjyl@gmail.com>
2024-04-24 12:27:56 -0700

    Integrating Geolocation

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -5,10 +5,11 @@ import { Form, redirect, useNavigation, useActionData } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
 import EmptyCart from '../cart/EmptyCart'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { clearCart, getCart, getTotalPrice } from '../cart/cartSlice'
 import store from '../../store'
 import { formatCurrency } from '../../utils/helpers'
+import { fetchAddress } from '../user/userSlice'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -19,11 +20,19 @@ const isValidPhone = (str) =>
 function CreateOrder() {
     const [withPriority, setWithPriority] = useState(false)
     const navigation = useNavigation()
+    const dispatch = useDispatch()
     const isSubmitting = navigation.state === 'submitting'
     const formErrors = useActionData()
 
-    const { username } = useSelector((store) => store.user)
+    const {
+        username,
+        status: addressStatus,
+        position,
+        address,
+        error: errorAddress,
+    } = useSelector((store) => store.user)
 
+    const isLoadingAddress = addressStatus === 'loading'
     const cart = useSelector(getCart)
     const totalCartPrice = useSelector(getTotalPrice)
     const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0
@@ -66,16 +75,37 @@ function CreateOrder() {
                     </div>
                 </div>
 
-                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
                     <label className="sm:basis-40">Address</label>
                     <div className="grow">
                         <input
                             className="input w-full"
                             type="text"
                             name="address"
+                            disabled={isLoadingAddress}
+                            defaultValue={address}
                             required
                         />
+                        {addressStatus === 'error' && (
+                            <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                                {errorAddress}
+                            </p>
+                        )}
                     </div>
+                    {!position.latitude && !position.longtitude && (
+                        <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+                            <Button
+                                disabled={isLoadingAddress}
+                                type="small"
+                                onClick={(e) => {
+                                    e.preventDefault()
+                                    dispatch(fetchAddress())
+                                }}
+                            >
+                                Get Position
+                            </Button>
+                        </span>
+                    )}
                 </div>
 
                 <div className="mb-12 flex items-center gap-5">
@@ -98,7 +128,19 @@ function CreateOrder() {
                         name="cart"
                         value={JSON.stringify(cart)}
                     />
-                    <Button disabled={isSubmitting} type="primary">
+                    <input
+                        type="hidden"
+                        name="position"
+                        value={
+                            position.longtitude && position.latitude
+                                ? `${position.latitude}, ${position.longtitude}`
+                                : ''
+                        }
+                    />
+                    <Button
+                        disabled={isSubmitting || isLoadingAddress}
+                        type="primary"
+                    >
                         {isSubmitting
                             ? 'Placing order now'
                             : `Order now for ${formatCurrency(totalPrice)}`}
