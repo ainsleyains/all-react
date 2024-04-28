bcd31bd3e19525c44db02fc9445988a83314aaf2
Ainsley <ainsleyjyl@gmail.com>
2024-04-28 14:07:56 -0700

    add reuseable components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -2,19 +2,41 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Heading from './ui/Heading';
 import Input from './ui/Input';
+import Row from './ui/Row';
 
 export default function App() {
     return (
         <>
             <GlobalStyles />
-            <div className=''>
-                <Heading as='h1'>The Wild Oasis</Heading>
-                <Button>Check in</Button>
-                <Input
-                    type='number'
-                    placeholder='Number'
-                />
-            </div>
+            <Row>
+                <Row type='horizontal'>
+                    <Heading as='h1'>The Wild Oasis</Heading>
+                    <div>
+                        <Heading as='h2'>Check in and out</Heading>
+                        <Button
+                            variation='primary'
+                            size='medium'
+                        >
+                            Check in
+                        </Button>
+                        <Button
+                            variation='secondary'
+                            size='small'
+                        >
+                            Check out
+                        </Button>
+                    </div>
+                </Row>
+                <Row>
+                    <Heading as='h3'>Form</Heading>
+                    <form action=''>
+                        <Input
+                            type='number'
+                            placeholder='Number'
+                        />
+                    </form>
+                </Row>
+            </Row>
         </>
     );
 }
