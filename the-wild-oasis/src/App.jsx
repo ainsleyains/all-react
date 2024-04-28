7816e9d3b3b7201d0e3ab7e3ef375b60e5cfe235
Ainsley <ainsleyjyl@gmail.com>
2024-04-27 21:02:44 -0700

    add styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,3 +1,19 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    background-color: yellow;
+`;
+
+const Button = styled.button`
+    font-size: 1.4rem;
+`;
 export default function App() {
-    return <div className=''>Hello, world</div>;
+    return (
+        <div className=''>
+            <H1>The World Oasis</H1>
+            <Button>Check in</Button>
+        </div>
+    );
 }
