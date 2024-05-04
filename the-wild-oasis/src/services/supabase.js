d092a37c99c82b5576059dcb7ff12bcf6dec7e45
Ainsley <ainsleyjyl@gmail.com>
2024-05-03 20:20:07 -0700

    connect supabase with the app

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://jntaitnipgiuimjiijke.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpudGFpdG5pcGdpdWltamlpamtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3NjExNTQsImV4cCI6MjAzMDMzNzE1NH0.xR1HwYnv_5xVYi5WIJhrj-2PkXdy2K-xu4nXWE3Vggk';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
