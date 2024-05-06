cb5db158138f109afb7319f0ae9a6aa2e09ae156
Ainsley <ainsleyjyl@gmail.com>
2024-05-06 11:22:13 -0700

    delete cabin data

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteCabin } from '../../services/apiCabins';
 
 const TableRow = styled.div`
     display: grid;
@@ -42,8 +44,20 @@ const Discount = styled.div`
 `;
 
 export default function CabinRow({ cabin }) {
-    const { name, maxCapacity, regularPrice, discount, image } = cabin;
-    console.log(image);
+    const { id: cabinId, name, maxCapacity, regularPrice, discount, image } = cabin;
+
+    const queryClient = useQueryClient();
+
+    const { isLoading: isDeleting, mutate } = useMutation({
+        mutationFn: deleteCabin,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['cabins'],
+            });
+        },
+        onError: (err) => alert(err.message),
+    });
+
     return (
         <TableRow role='row'>
             <Img src={image} />
@@ -51,7 +65,12 @@ export default function CabinRow({ cabin }) {
             <div>Fits up to {maxCapacity} guests</div>
             <Price>{formatCurrency(regularPrice)}</Price>
             <Discount>{formatCurrency(discount)}</Discount>
-            <button>Delete</button>
+            <button
+                onClick={() => mutate(cabinId)}
+                disabled={isDeleting}
+            >
+                Delete
+            </button>
         </TableRow>
     );
 }
