8bd0a179892a55c6f98234f27de9942d04989378
Ainsley <ainsleyjyl@gmail.com>
2024-05-20 18:11:30 -0700

    uploading image to supabase

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://jntaitnipgiuimjiijke.supabase.co';
+export const supabaseUrl = 'https://jntaitnipgiuimjiijke.supabase.co';
 const supabaseKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpudGFpdG5pcGdpdWltamlpamtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ3NjExNTQsImV4cCI6MjAzMDMzNzE1NH0.xR1HwYnv_5xVYi5WIJhrj-2PkXdy2K-xu4nXWE3Vggk';
 const supabase = createClient(supabaseUrl, supabaseKey);
