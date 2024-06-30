31a249dc63036f936d0a905014d8d60ee6164d22
Ainsley <ainsleyjyl@gmail.com>
2024-06-29 19:12:11 -0700

    Defining Routes and Pages

---
+++ the-wild-oasis-web/app/about/page.js
@@ -0,0 +1,3 @@
+export default function page() {
+    return <div className=''>About Wild Oasis</div>;
+}
---
+++ the-wild-oasis-web/app/account/page.js
@@ -0,0 +1,3 @@
+export default function page() {
+    return <div className=''>Your account</div>;
+}
